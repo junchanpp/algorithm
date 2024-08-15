478eaccd2f00059d6a72360dd91d9427bb2cf91a
junchanpp <49396352+junchanpp@users.noreply.github.com>
2024-08-15 21:08:03 +0900

    [Silver II] Title: 파닭파닭, Time: 356 ms, Memory: 5928 KB -BaekjoonHub

--- 백준/Silver/14627. 파닭파닭/파닭파닭.cc
+++ 백준/Silver/14627. 파닭파닭/파닭파닭.cc
@@ -4,42 +4,38 @@
 
 using namespace std;
 
-long long S, C;
-vector<long long> L;
-long long sum = 0;
+int s, c;
+vector<int> arr(1000005);
 
-bool check(long long num){
-    long long result = 0;
-    for(int i = 0 ; i < S; i++){
-        result += L[i] / num;
+bool check(long long mid) {
+    long long cnt = 0;
+    for(int i = 0; i < s; i++) {
+        cnt += arr[i] / mid;
     }
-    return result >= C;
+    return cnt >= c;
 }
 
-int main(){
+int main() {
+    ios::sync_with_stdio(false);
     cin.tie(0);
-    ios_base::sync_with_stdio(0);
-    cin >> S >> C;
-    L.assign(S, 0);
-    for(int i = 0; i < S; i++){
-        cin >> L[i];
-        sum += L[i];
+
+    cin >> s >> c;
+
+    long long total = 0;
+    for(int i = 0; i < s; i++) {
+        cin >> arr[i];
+        total += arr[i];
     }
 
-    sort(L.begin(), L.end());
-
-    long long hi = 1000000000;
-    long long lo = 1;
-    long long target = 0;
-    while(lo <= hi){
-        long long mid = (lo+hi)/2;
-        if(check(mid)){//check(): [mid, S-1]까지 C개 이상이면 true
-            lo = mid + 1;
-            target = mid;
-        } else {
-            hi = mid - 1;
-        }
+    long long lo = 0;
+    long long hi = 1e9 + 5;
+    while(lo + 1 < hi) {
+        long long mid = (lo + hi) / 2;
+        if(check(mid)) lo = mid;
+        else hi = mid;
     }
-    cout << sum - (target * C);
 
+    cout << total - lo * c << endl;
+
+    return 0;
 }
