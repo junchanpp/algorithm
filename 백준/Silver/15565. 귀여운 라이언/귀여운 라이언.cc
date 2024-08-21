927e4ba8f30908983b40195e5090cb9eed068f42
junchanpp <49396352+junchanpp@users.noreply.github.com>
2024-08-21 13:32:36 +0900

    [Silver I] Title: 귀여운 라이언, Time: 156 ms, Memory: 5908 KB -BaekjoonHub

---
+++ 백준/Silver/15565. 귀여운 라이언/귀여운 라이언.cc
@@ -0,0 +1,49 @@
+#include <iostream>
+#include <vector>
+#include <climits>
+
+using namespace std;
+
+int N, K;
+vector<int> inputs;
+
+int main(){
+    cin >> N >> K;
+    inputs.resize(N);
+
+
+    for(int i = 0 ; i < N; i++){
+        cin >> inputs[i];
+        --inputs[i];
+    }
+
+    int count = 0;
+    int answer = INT_MAX;
+
+    int i = 0;
+    int j = 0;
+    
+
+    //i는 0, j는 
+    while(j < N){
+        while(count < K && j < N){
+            count += inputs[j++] ? 0 : 1;
+        }
+        if(count < K && j == N){
+            break;
+        }
+        j--;
+        while(inputs[i]){
+            i++;
+        }
+        if(answer > j - i + 1){
+            answer = j - i + 1;
+        }
+        i++;
+        j++;
+        count--;
+    }
+
+    cout << (answer == INT_MAX ? -1 : answer);
+
+}
