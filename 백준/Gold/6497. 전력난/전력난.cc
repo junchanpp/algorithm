4086070be3f2adcd45a1b4f8fcd041c755bc70ab
junchanpp <49396352+junchanpp@users.noreply.github.com>
2024-10-26 15:29:54 +0900

    [Gold IV] Title: 전력난, Time: 860 ms, Memory: 6680 KB -BaekjoonHub

---
+++ 백준/Gold/6497. 전력난/전력난.cc
@@ -0,0 +1,91 @@
+#include <iostream>
+#include <vector>
+#include <algorithm>
+
+using namespace std;
+
+
+
+typedef struct Node{
+    int distance;
+    int x;
+    int y;
+
+    Node(int distance, int x, int y): distance(distance), x(x), y(y){}
+}Node;
+
+bool compare(const Node& a, const Node& b){
+    return a.distance < b.distance;
+}
+
+vector<Node> nodes;
+ 
+vector<int> parents;
+
+int M, N;
+
+
+
+int find(int x){
+    if(parents[x] < 0){
+        return x;
+    }
+    return parents[x] = find(parents[x]);
+}
+
+bool isEqualParent(int x, int y){
+    return find(x) == find(y);
+}
+
+void merge(int x, int y){
+    x = find(x);
+    y = find(y);
+    if(parents[x] <= parents[y]){
+        parents[x] += parents[y];
+        parents[y] = x;
+    }else{
+        parents[y] += parents[x];
+        parents[x] = y;
+    }
+}
+
+int main(){
+    
+    while(1){
+        cin >> M >> N;
+        if(N == 0 && M == 0){
+            return 0;
+        }
+
+        parents.clear();
+        parents.assign(M, -1);
+
+        nodes.clear();
+
+        for(int i = 0; i < N; i++){
+            int x, y, z;
+            cin >> x >> y >> z;
+            nodes.push_back(Node(z, x, y));
+        }
+
+        sort(nodes.begin(), nodes.end(), compare);
+
+
+        int result = 0;
+
+        for(int i = 0; i < nodes.size(); i++){
+            int x = nodes[i].x;
+            int y = nodes[i].y;
+            int distance = nodes[i].distance;
+            if(isEqualParent(x,y)){
+                result += distance;
+            }else {
+                merge(x,y);
+            }
+        }
+
+        cout << result << '\n';
+    }
+
+    return 0;
+}
