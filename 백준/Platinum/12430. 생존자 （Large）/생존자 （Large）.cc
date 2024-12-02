b348cebfa6ac8e1777b4ad7c635472b456260262
junchanpp <49396352+junchanpp@users.noreply.github.com>
2024-12-02 12:55:22 +0900

    [Platinum IV] Title: 생존자 (Large), Time: 3668 ms, Memory: 2944 KB -BaekjoonHub

---
+++ 백준/Platinum/12430. 생존자 （Large）/생존자 （Large）.cc
@@ -0,0 +1,52 @@
+#include <iostream>
+#include <vector>
+#include <utility>
+#include <algorithm>
+#include <cmath>
+
+using namespace std;
+
+int T, N;
+vector<int> answers;
+
+int main(){
+    cin.tie(0);
+    cout.tie(0);
+    ios_base::sync_with_stdio(0);
+    cin >> T;
+    while(T--){
+        vector<pair<int, int> > inputs;
+        cin >> N;
+
+        for(int i = 0 ; i < N; i++){
+            int P, S;
+            cin >> P >> S;
+            inputs.push_back(make_pair((S + P), S));
+        }
+
+        sort(inputs.begin(), inputs.end());
+
+        vector<int> times;
+        times.assign(200010, 0);
+        times[0] = 1;
+
+        int answer = 0;
+        for(int i = 0; i < inputs.size(); i++){
+            int end = inputs[i].first - inputs[i].second;
+            int eatTime = inputs[i].second;
+            for(int i = end; i >= 0; i--){
+                if(times[i]){
+                    times[i+eatTime]= 1;
+                    answer = max(answer, i+ eatTime);
+                }
+            }
+        }
+        answers.push_back(answer);
+
+    }
+
+    for(int i = 0; i < answers.size(); i++){
+        cout << "Case #" << (i+1) << ": " << answers[i] << '\n';
+    }
+    
+}
